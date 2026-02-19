fc2b07f70d654d3ebe041164d67ff16c7b36c6c9
Rohan Bhandari <rohanbhandari124@gmail.com>
2026-02-19 04:59:24 -0600

    Added printInventory and simple main

---
+++ Grocery.java
@@ -0,0 +1,24 @@
+package CS3354_Assignment1;
+
+public class Grocery {
+    public static void printInventory(String[] names, double[] prices, int[] stocks){
+    for (int i=0; i<names.length;i++){
+    if (names[i] != null){
+    System.out.println("Name: " + names[i]);
+    System.out.println("Price: " + prices[i]);
+    System.out.println("Stock: " + stocks[i]);
+    }
+    }   
+}
+public static void main(String[] args) {
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+
+        itemNames[0] = "Apple";
+        itemPrices[0] = 0.99;
+        itemStocks[0] = 10;
+
+        printInventory(itemNames, itemPrices, itemStocks);
+    }
+}
