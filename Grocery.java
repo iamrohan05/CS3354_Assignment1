f0edba76cc714532d64d2c58f43ed497d8b9691b
Rohan Bhandari <rohanbhandari124@gmail.com>
2026-02-19 05:19:26 -0600

    Updated main function

--- Grocery.java
+++ Grocery.java
@@ -1,5 +1,7 @@
 package CS3354_Assignment1;
 
+import java.util.Scanner;
+
 public class Grocery {
     public static void printInventory(String[] names, double[] prices, int[] stocks){
     for (int i=0; i<names.length;i++){
@@ -30,6 +32,44 @@ public static void main(String[] args) {
         itemPrices[0] = 0.99;
         itemStocks[0] = 10;
 
-        printInventory(itemNames, itemPrices, itemStocks);
+        itemNames[1] = "Bread";
+        itemPrices[1] = 2.49;
+        itemStocks[1] = 15;
+
+        itemNames[2] = "Milk";
+        itemPrices[2] = 3.29;
+        itemStocks[2] = 10;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n--- Grocery Menu ---");
+            System.out.println("1. View Inventory");
+            System.out.println("2. Restock Item");
+            System.out.println("3. Exit");
+            System.out.print("Enter choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+
+            if (choice == 1) {
+                printInventory(itemNames, itemPrices, itemStocks);
+            } else if (choice == 2) {
+                System.out.print("Enter item name: ");
+                String target = scanner.nextLine();
+
+                System.out.print("Enter amount to add: ");
+                int amount = scanner.nextInt();
+
+                restockItem(itemNames, itemStocks, target, amount);
+            } else if (choice == 3) {
+                System.out.println("Exiting program.");
+                break;
+            } else {
+                System.out.println("Invalid choice. Try again.");
+            }
+        }
+
+        scanner.close();
     }
 }
